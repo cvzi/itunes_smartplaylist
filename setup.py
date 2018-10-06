951b14786fc7d1ce28ac7568f22c3e62cfc39bc0
cvzi <cuzi@openmail.cc>
2018-10-06 22:59:06 +0200

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setuptools.setup(
     classifiers=(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
