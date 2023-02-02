5e35dad5b640f68c81243bb78574c6960f7e8f27
cvzi <cuzi@openmail.cc>
2023-02-02 18:06:06 +0100

    Replace setup.py with pyproject.toml

--- itunessmart/__init__.py
+++ itunessmart/__init__.py
@@ -2,7 +2,7 @@
 iTunes Smart playlist parser with Python. Decode itunes smart playlist rules from base64 data. Convert iTunes smart playlists to Kodi xsp smart playlists.
 """
 
-__version__ = '1.1.5'
+from ._version import __version__
 __author__ = 'cuzi'
 __email__ = 'cuzi@openmail.cc'
 __source__ = 'https://github.com/cvzi/itunes_smartplaylist'
--- setup.py
+++ setup.py
@@ -1,43 +1,9 @@
-﻿import setuptools
-import os
-import io
+﻿#!/usr/bin/env python
 
-with io.open("README.md", encoding="utf-8") as f:
-    long_description = f.read().strip()
+from setuptools import setup
 
-version = None
-with io.open(os.path.join("itunessmart", "__init__.py"), encoding="utf-8") as f:
-    for line in f:
-        if line.strip().startswith("__version__"):
-            version = line.split("=")[1].strip().replace('"', "").replace("'", "")
-            break
-
-setuptools.setup(
-    name="itunessmart",
-    version=version,
-    license="MIT",
-    author="cuzi",
-    author_email="cuzi@openmail.cc",
-    description="Decode iTunes Smart playlist rules. Convert iTunes smart playlists to Kodi xsp smart playlists.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cvzi/itunes_smartplaylist",
-    packages=["itunessmart"],
-    package_dir={"itunessmart": "itunessmart"},
-    zip_safe=True,
-    test_suite="nose.collector",
-    tests_require=["nose"],
-    classifiers=(
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Topic :: Multimedia :: Sound/Audio",
-        "Topic :: Multimedia :: Sound/Audio :: Players"
+if __name__ == "__main__":
+    setup(
+        name="itunessmart",
+        requires=[],
     )
-)
