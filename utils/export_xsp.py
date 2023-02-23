34e5674b3fb039c7dc289850a0a422e07334716a
cvzi <cuzi@openmail.cc>
2023-02-23 21:17:39 +0100

    Always try to import from parent directory Same as 4fc6f349c03a4b09df055cee258d6f907f466772

--- utils/export_xsp.py
+++ utils/export_xsp.py
@@ -3,14 +3,10 @@
 """Export smart playlists from "iTunes Music Library.xml" to xsp files for Kodi"""
 
 import sys
-try:
-    import itunessmart.__main__
-except ImportError:
-    import os
-    include = os.path.relpath(os.path.join(os.path.dirname(__file__), ".."))
-    sys.path.insert(0, include)
-    import itunessmart.__main__
-    print("# Imported itunessmart from %s" % os.path.abspath(os.path.join(include, "itunessmart")))
+import os
+include = os.path.relpath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, include)
+import itunessmart.__main__
 
 if __name__ == "__main__":
     itunessmart.__main__.main(*sys.argv[1:])
